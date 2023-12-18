12e70cffa5d387a9033302a1bb1aab3c5b203492
Anthony Griffon <anthony@griffon.one>
2023-12-18 18:57:57 +0100

    update template
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,16 +1,15 @@
-import { helloMom } from "./application/mom";
+import { helloMom } from "./application/mom.js";
 
 type OnRequestType = {
   req: FetchEvent;
 }
-
 /**
  * Run each time a Request comes in.
  * You'll have a certain time to answer the request.
  * A Worker is
  */
 async function onRequest({ req }: OnRequestType) {
-  req.respondWith(new Response(new Blob([helloMom()]), {
+  req.respondWith(new Response(helloMom(), {
     status: 200,
   }))
 }
